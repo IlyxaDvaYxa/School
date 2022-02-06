a5216977c914db40046a105a85dde494b265a142
IlyxaDvaYxa <92630708+IlyxaDvaYxa@users.noreply.github.com>
2022-02-06 21:20:42 +0200

    update Valanta

--- Урок 30/index.js
+++ Урок 30/index.js
@@ -1,4 +1,4 @@
-let name=prompt("Input yout name:");
+/* let name=prompt("Input yout name:");
 let backColor=prompt(name + " Input color for background :");
 let mainHeading = document.querySelector("h1");
 let buttonFirst = document.querySelector(".button-first");
@@ -39,3 +39,82 @@ function changeColor2(){
     buttonFirst.style.transition="all 1s";
     buttonSecond.style.transition="all 1s";
 }
+ */
+// Exercises
+
+/* let num = 3;
+alert(num) */
+
+/* let a = 10;
+let b = 2;
+alert(+a + b) */
+
+/* let c = 15;
+let d = 2;
+let result = +c + d;
+alert (result) */
+
+/* let a =10;
+let b = 2;
+let c = 5;
+alert(+a + b + c) */
+
+/* let a = 17;
+let b = 10;
+let c = a - b
+let d = 7;
+let result = +c + d;
+alert(result) */
+
+/* let str = 'Привет, Мир!';
+alert (str) */
+
+/* let str1 ='Привет, ';
+let str2 ='Мир!';
+alert (str1 + str2); */
+
+/* let name = "Илья";
+alert('Привет, ' + name +"!") */
+
+/* let age = 15;
+alert ('Мне '+ age +' лет!') */
+
+/* let name = prompt("Ваше имя:")
+alert ('Ваше имя ' + name) */
+
+/* let number = prompt("Введите число:")
+alert(number ** 2) */
+
+/* let str = "abcde"
+alert(str[0])
+alert(str[2])
+alert(str[4]) */
+
+/* let num = "12345"
+alert(num[0]*num[1]*num[2]*num[3]*num[4]) */
+
+/* let hour = 60 * 60;
+let day = 60 * 60 *24;
+let month = 60 * 60 * 24 * 31;
+alert(hour + ' , ' +  day  +" , " +  month) */
+
+/* let hour=prompt("Введите часы");
+let minute=prompt("Введите минуты:");
+let second=prompt("Введите секунды:");
+alert(hour + ':' +  minute  +":" +  second); */
+
+/* let number = 15;
+alert(number ** 2) */
+
+/* var num = 47;
+num += 7;
+num == 18;
+num *= 10;
+num /= 15;
+alert(num); */
+
+/* var num = 10;
+num++;
+++num;
+--num;
+alert(num); */
--- Урок 31/index.js
+++ Урок 31/index.js
@@ -1,1 +1,126 @@
-document.write(145)
+/* let a = prompt("Введите а:")
+if ( a==0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a>0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a<0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a>=0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a<=0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a!=0 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:")
+if ( a=="test"){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = 1;
+let a = "1";
+if ( a==1 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let test = false;
+if ( test==true ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+}
+
+let test = false;
+test= test==true ? ("Верно") : ("Неверно");
+alert (test); */
+
+/* let test = false;
+if ( test != true ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+}
+
+let test = true;
+test= test!=true ? ("Верно") : ("Неверно");
+alert (test); */
+
+/* let a = prompt("Введите а:");
+if ( a>0 && a<5 ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+} */
+
+/* let a = prompt("Введите а:");
+if ( a==0 || a==2 ){
+    a=(+a + 7)
+}
+else{
+    a/=10
+}
+alert(a) */
+
+/* let a = prompt("Введите а:");
+let b = prompt("Введите b:");
+if ( a<=1 || b>=3 ){
+    c=(+a + +b)
+}
+else{
+    c=(a - b)
+}
+alert(c) */
+
+/* let a = prompt("Введите а:");
+let b = prompt("Введите b:");
+if ( (a>2 && a<11) || (b>=6 && b<14) ){
+    alert("Верно")
+}
+else{
+    alert("Неверно")
+}
+alert(c) */
+
